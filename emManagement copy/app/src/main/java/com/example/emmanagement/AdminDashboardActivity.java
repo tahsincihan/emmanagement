5e75147fdebaadad67fdb56f29aaf85dcdd09df6
Tahsin Cihan <132820140+tahsincihan@users.noreply.github.com>
2025-01-06 10:19:49 +0000

    xml done

---
+++ emManagement copy/app/src/main/java/com/example/emmanagement/AdminDashboardActivity.java
@@ -0,0 +1,13 @@
+package com.example.emmanagement;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
---
+++ emManagement copy/app/src/main/java/com/example/emmanagement/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.emmanagement;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Method for navigating to Admin Dashboard
+    public void navigateToAdminDashboard(View view) {
+        Intent intent = new Intent(this, AdminDashboardActivity.class);
+        startActivity(intent);
+    }
+}
